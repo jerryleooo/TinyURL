f7094ad49cce303b1249af61c0a43ddb7bb809c7
whilgeek <whilgeek@gmail.com>
2017-04-28 13:15:20 +0800

    all env

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	_ "tinyUrl/routers"
+	"fmt"
 	"github.com/astaxie/beego"
+	"os"
+	"strings"
+	_ "tinyUrl/routers"
 )
+
+func init() {
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair)
+	}
+}
 
 func main() {
 	beego.Run()
 }
-
