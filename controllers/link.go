05d5379efe18552ecb77cce98501b85b5ff8730c
whilgeek <whilgeek@gmail.com>
2017-05-17 20:35:25 +0800

    add click count

--- controllers/link.go
+++ controllers/link.go
@@ -14,5 +14,7 @@ func (c *LinkController) Get() {
 	shortId := c.GetString(":link")
 	fmt.Println(shortId)
 	url := models.UrlDecode(shortId)
+	clickCount := models.IncrClickCount(shortId)
+	fmt.Println(clickCount)
 	c.Redirect(url, 302)
 }
