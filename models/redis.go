3a7b3efc6d3ccf73c6e5abf72a7961d492878711
whilgeek <whilgeek@gmail.com>
2017-05-17 18:09:15 +0800

    fix some bug

--- models/redis.go
+++ models/redis.go
@@ -3,31 +3,20 @@ package models
 import (
 	"github.com/go-redis/redis"
 	"os"
-	"strings"
 )
 
 var Client *redis.Client
 
-func NewClient(host string, port string) *redis.Client {
-	addr := []string{host, port}
-	client := redis.NewClient(&redis.Options{
-		Addr:     strings.Join(addr, ":"),
-		Password: "",
-		DB:       0,
-	})
+func NewClient() *redis.Client {
+	var redisUrl string
+	if redisUrl = os.Getenv("REDIS_URL"); redisUrl == "" {
+		redisUrl = "redis://127.0.0.1:6379"
+	}
+	options, _ := redis.ParseURL(redisUrl)
+	client := redis.NewClient(options)
 	return client
 }
 
 func init() {
-	var redisHost, redisPort string
-	redisHost = os.Getenv("REDIS_HOST")
-	if redisHost == "" {
-		redisHost = "127.0.0.1"
-	}
-
-	redisPort = os.Getenv("REDIS_PORT")
-	if redisPort == "" {
-		redisPort = "6379"
-	}
-	Client = NewClient(redisHost, redisPort)
+	Client = NewClient()
 }
